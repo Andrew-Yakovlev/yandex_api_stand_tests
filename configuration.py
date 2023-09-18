04309ec5529369a581408a31b550c64e816987a4
artavod <basten1234@yandex.ru>
2023-09-18 15:37:57 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://ca4ef18a-c34f-45ee-9674-5dab3a23a59b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
